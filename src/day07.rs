736fd39d2fb510cc5c83fbc12774160f640e21a3
alex800121 <alex800121@hotmail.com>
2024-04-03 06:49:36 +0800

    day08

--- src/day07.rs
+++ src/day07.rs
@@ -1,12 +1,47 @@
+use itertools::Itertools;
 use project_root::get_project_root;
+use std::cmp::Ordering::*;
 
-fn bi_binary()
-
+fn bin_search<T: Fn(&Vec<i32>, i32) -> i32>(v: &Vec<i32>, f: T) -> i32 {
+    let mut hi = *v.iter().max().unwrap();
+    let mut lo = *v.iter().min().unwrap();
+    while lo < hi {
+        let mid = (hi + lo) / 2;
+        match f(v, mid).cmp(&f(v, mid + 1)) {
+            Less => {
+                hi = mid;
+            }
+            _ => {
+                lo = mid + 1;
+            }
+        }
+    }
+    f(v, lo)
+}
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let v = input
+        .trim()
+        .split(',')
+        .filter_map(|x| x.parse::<i32>().ok())
+        .collect_vec();
+    println!(
+        "day7a: {}",
+        bin_search(&v, |xs, y| xs.iter().map(|x| (x - y).abs()).sum::<i32>())
+    );
+    println!(
+        "day7b: {}",
+        bin_search(&v, |xs, y| xs
+            .iter()
+            .map(|x| {
+                let a = (x - y).abs();
+                (a * (a + 1)) / 2
+            })
+            .sum::<i32>())
+    );
 }
--- src/day08.rs
+++ src/day08.rs
@@ -1,6 +1,21 @@
+use itertools::Itertools;
 use project_root::get_project_root;
 
+const SIG: [u8; 7] = [0, 1, 2, 3, 4, 5, 6];
 pub fn run(day: usize) {
+    let num = [
+        vec![0, 1, 2, 4, 5, 6],
+        vec![2, 5],
+        vec![0, 2, 3, 4, 6],
+        vec![1, 2, 3, 5],
+        vec![0, 1, 3, 5, 6],
+        vec![0, 1, 3, 4, 5, 6],
+        vec![0, 2, 5],
+        vec![0, 1, 2, 3, 4, 5, 6],
+        vec![0, 1, 2, 3, 5, 6],
+    ];
+    let perm = SIG.iter().permutations(7);
+    dbg!(num);
     // let input = std::fs::read_to_string(format!(
     //     "{}/input/input{:02}.txt",
     //     get_project_root().unwrap().to_str().unwrap(),
