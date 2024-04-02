c1a449d991e93677c586590d3b24b5a151d131af
alex800121 <alex800121@hotmail.com>
2024-04-02 19:42:08 +0800

    day04

--- src/day04.rs
+++ src/day04.rs
@@ -4,11 +4,38 @@ use project_root::get_project_root;
 type BingoBoard = [[(u32, bool); 5]; 5];
 
 fn calc_board(board: BingoBoard, n: u32) -> u32 {
-    unimplemented!()
+    board
+        .iter()
+        .map(|x| x.iter().map(|(a, b)| if !b { *a } else { 0 }).sum::<u32>())
+        .sum::<u32>()
+        * n
 }
-fn bingo<T: Iterator<Item = u32>>(boards: Vec<BingoBoard>, called: T) -> Vec<u32> {
+fn bingo(board: &BingoBoard) -> bool {
+    for i in 0..5 {
+        if board[i].iter().all(|x| x.1) || board.iter().all(|v| v[i].1) {
+            return true;
+        }
+    }
+    false
+}
+fn fill_n(board: &mut BingoBoard, called: u32) {
+    board.iter_mut().for_each(|x| {
+        x.iter_mut().for_each(|a| {
+            if a.0 == called {
+                a.1 = true;
+            }
+        })
+    });
+}
+fn call<T: Iterator<Item = u32>>(boards: &mut Vec<BingoBoard>, called: T) -> Vec<u32> {
     let mut scores = Vec::new();
-    for c in called {}
+    for c in called {
+        boards.iter_mut().for_each(|b| fill_n(b, c));
+        let (bingoed, b): (Vec<_>, Vec<_>) = boards.iter().partition(|x| bingo(x));
+        let bingoed = bingoed.iter().map(|x| calc_board(*x, c));
+        scores.extend(bingoed);
+        *boards = b;
+    }
     scores
 }
 pub fn run(day: usize) {
@@ -18,25 +45,31 @@ pub fn run(day: usize) {
         day
     ))
     .unwrap();
-    let (called, boards) = {
+    let (called, mut boards) = {
         let (a, b) = input.split_once("\n\n").unwrap();
         let boards: Vec<BingoBoard> = b
             .split("\n\n")
             .map(|b| {
                 b.split('\n')
-                    .map(|b| {
+                    .filter_map(|b| {
                         b.split_whitespace()
                             .filter_map(|b| b.parse::<u32>().ok().map(|b| (b, false)))
                             .collect_vec()
                             .try_into()
-                            .unwrap()
+                            .ok()
                     })
                     .collect_vec()
                     .try_into()
                     .unwrap()
             })
             .collect_vec();
-        (a.split(", ").filter_map(|x| x.parse::<u32>().ok()), boards)
+        (
+            a.split(',')
+                .filter_map(|x| x.parse::<u32>().ok()),
+            boards,
+        )
     };
-    dbg!(called, boards);
+    let scores = call(&mut boards, called);
+    println!("day4a: {}", scores.first().unwrap());
+    println!("day4a: {}", scores.last().unwrap());
 }
