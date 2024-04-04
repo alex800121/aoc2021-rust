e24e45734529d17440c45a9fb0e6bfcc8580b3cf
alex800121 <alex800121@hotmail.com>
2024-04-04 19:16:48 +0800

    day12

--- src/day12.rs
+++ src/day12.rs
@@ -1,10 +1,19 @@
 use project_root::get_project_root;
+use petgraph::{Graph, graph::UnGraph, data::Build};
 
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let mut g: UnGraph<&str, ()> = Graph::new_undirected();
+    let mut nodes = Vec::new();
+    for s in input.lines() {
+        if let Some((a, b)) = s.split_once('-') {
+            let mut x;
+            let mut y;
+        }
+    }
 }
